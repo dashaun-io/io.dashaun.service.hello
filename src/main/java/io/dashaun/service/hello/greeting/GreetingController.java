b09d04272521d077b27fadc53ea3354e0a19b274
dashaun <dashaun@dashaun.com>
2024-04-30 15:44:42 -0500

    hello

---
+++ src/main/java/io/dashaun/service/hello/greeting/GreetingController.java
@@ -0,0 +1,19 @@
+package io.dashaun.service.hello.greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	Greeting greeting(@RequestParam(defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), template.formatted(name));
+	}
+}
---
+++ src/main/java/io/dashaun/service/hello/greeting/package-info.java
@@ -0,0 +1,2 @@
+@org.springframework.lang.NonNullApi
+package io.dashaun.service.hello.greeting;
